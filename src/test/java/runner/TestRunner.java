06c224baba3ad70e93d0d3e56df63718d5a6f6c1
Hovhannes_Grigoryan <Hovhannes_Grigoryan@epam.com>
2021-06-10 21:39:24 +0400

    made changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,8 +3,9 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(
-        features = {"src/test/java/features"})
+        features = {"src/test/java/features"},plugin = {"json:target/cucumber.json","html:target/site/cucumber-pretty"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
